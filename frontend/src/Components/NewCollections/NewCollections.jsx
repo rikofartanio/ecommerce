a81ef4283404313bf3c15806bad9b7505b2e1bbd
rikofartanio <rikofartanio@gmail.com>
2025-02-23 02:27:33 +0700

    Update NewCollections.jsx

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection,setNew_collection] = useState([]);
 
   useEffect(()=>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://api.shopmart10.shop/newcollections')
     .then((response)=>response.json())
     .then((data)=>setNew_collection(data));
   },[])
